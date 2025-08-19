2b9e256c06e25cc9eb36a5d7347edd294d4023ef
William01br <williamstcorreia123@gmail.com>
2025-08-19 09:05:47 -0300

    feat: add UserMapper

---
+++ src/shared/mappers/user.mapper.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { UserEntity } from 'src/users/entities/user.entity';
+
+type PrismaUserWithProfiles = Prisma.UserGetPayload<{
+  include: {
+    elderProfile: true;
+    caregiverProfile: true;
+  };
+}>;
+
+@Injectable()
+export class UserMapper {
+  toEntityFromPrisma(user: PrismaUserWithProfiles): UserEntity {
+    return {
+      id: user.id,
+      cpf: user.cpf,
+      email: user.email,
+      name: user.name,
+      phone: user.phone,
+      role: user.role as UserEntity['role'],
+      birthDate: user.birthDate,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      elderProfile: user.elderProfile
+        ? {
+            userId: user.elderProfile.userId,
+            createdAt: user.elderProfile.createdAt,
+            caregiverId: user.elderProfile.caregiverId,
+            bpm: user.elderProfile.bpm,
+          }
+        : null,
+      caregiverProfile: user.caregiverProfile
+        ? {
+            userId: user.caregiverProfile.userId,
+            createdAt: user.caregiverProfile.createdAt,
+          }
+        : null,
+    };
+  }
+}
