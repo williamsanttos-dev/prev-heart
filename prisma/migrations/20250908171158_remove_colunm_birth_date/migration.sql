1e32fd91205673ee0a4da95c5f9ba7c51dd487dc
William01br <williamstcorreia123@gmail.com>
2025-09-11 14:54:59 -0300

    feat: add migrations

---
+++ prisma/migrations/20250908171158_remove_colunm_birth_date/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `birth_date` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."user" DROP COLUMN "birth_date";
