94d20ea6265706eab2052e53a14e83752477599e
William01br <williamstcorreia123@gmail.com>
2025-09-11 14:57:55 -0300

    chore: add some packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { PushTokenModule } from './push-token/push-token.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule],
+  imports: [AuthModule, PrismaModule, UsersModule, PushTokenModule],
   controllers: [AppController],
   providers: [AppService],
 })
