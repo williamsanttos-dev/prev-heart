70f8997bbe61952e82d5110c8034f79056f319bb
William01br <williamstcorreia123@gmail.com>
2025-09-23 13:36:26 -0300

    chore: add swagger anotation for dtos users

--- src/users/dto/device-id.dto.ts
+++ src/users/dto/device-id.dto.ts
@@ -1,6 +1,8 @@
 import { Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class DeviceIdDTO {
+  @ApiProperty()
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8}$/, {
     message:
       'The device ID must be valid. It must contain only letters and numbers, and the length must be 8. ',
--- src/users/dto/heart-beat.dto.ts
+++ src/users/dto/heart-beat.dto.ts
@@ -1,8 +1,14 @@
 import { Type } from 'class-transformer';
 import { IsInt, Max, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class HeartBeatDTO {
-  @Type(() => Number) @IsInt() @Min(1) @Max(220) bpm: number;
+  @ApiProperty()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(220)
+  bpm: number;
 }
 
 export class HeartBeatResponseDTO {
