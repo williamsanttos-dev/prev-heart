4b16b6858e612e13b6e947f3be357c023d5c77f1
William01br <williamstcorreia123@gmail.com>
2025-10-16 10:08:21 -0300

    test: add unit tests for remove and sendBPM methods in users.service.ts

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -7,32 +7,36 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtPayloadDTO } from 'src/auth/dto/Jwt-payload';
 import { DeviceIdDTO } from './dto/device-id.dto';
 import { InternalServerErrorException } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { HeartBeatDTO, HeartBeatResponseDTO } from './dto/heart-beat.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
   let prisma: Partial<PrismaService>;
   let pushToken: Partial<PushTokenService>;
 
-  const mockUserEntity = {
+  const date = new Date('2025-09-08T17:25:18.802Z');
+  const mockUserEntity: UserEntity = {
     id: 1,
     cpf: '41579506070',
     email: 'johndoe123@example.com',
     name: 'John Doe',
     phone: '011980028922',
     role: 'elder',
-    createdAt: '2025-09-08T17:25:18.802Z',
-    updatedAt: '2025-09-08T17:25:18.802Z',
+    createdAt: date,
+    updatedAt: date,
     elderProfile: {
       userId: 1,
       caregiverId: 15,
       bpm: 72,
       deviceId: '24Wwsc24',
-      createdAt: '2025-09-08T17:25:18.805Z',
-      updatedAt: '2025-09-08T17:25:18.805Z',
+      createdAt: date,
+      updatedAt: date,
     },
     caregiverProfile: {
       userId: 1,
-      createdAt: '2025-09-08T17:25:18.805Z',
+      createdAt: date,
     },
   };
   const mockPayloadJwt: JwtPayloadDTO = {
@@ -45,6 +49,7 @@ describe('UsersService', () => {
       user: {
         findUnique: jest.fn(),
         update: jest.fn(),
+        deleteMany: jest.fn(),
       },
       elderProfile: {
         update: jest.fn(),
@@ -118,7 +123,7 @@ describe('UsersService', () => {
     });
   });
   describe('update', () => {
-    const mockUpdateUserDTO = {
+    const mockUpdateUserDTO: UpdateUserDto = {
       name: 'newJohnDoe',
       phone: '011980028944',
     };
@@ -156,6 +161,111 @@ describe('UsersService', () => {
 
       await expect(
         service.update(mockPayloadJwt, mockUpdateUserDTO),
+      ).rejects.toBe(unknownError);
+    });
+  });
+  describe('remove', () => {
+    it('happy path: call prisma.user.delete with correct where and resolves', async () => {
+      (prisma.user?.deleteMany as jest.Mock).mockResolvedValueOnce(
+        mockPayloadJwt.userId,
+      );
+
+      await expect(service.remove(mockPayloadJwt)).resolves.toBeUndefined();
+      expect(prisma.user?.deleteMany).toHaveBeenCalledTimes(1);
+      expect(prisma.user?.deleteMany).toHaveBeenCalledWith({
+        where: { id: mockPayloadJwt.userId },
+      });
+    });
+    it('deleteMany path: resolves even if no record', async () => {
+      (prisma.user?.deleteMany as jest.Mock).mockResolvedValueOnce({
+        count: 0,
+      });
+
+      await expect(service.remove(mockPayloadJwt)).resolves.toBeUndefined();
+      expect(prisma.user?.deleteMany).toHaveBeenCalledWith({
+        where: { id: mockPayloadJwt.userId },
+      });
+    });
+    it('should propagate other DB errors as InternalServerError (or original)', async () => {
+      const unknownError = new Error('connection error');
+      (prisma.user?.deleteMany as jest.Mock).mockRejectedValueOnce(
+        unknownError,
+      );
+
+      await expect(service.remove(mockPayloadJwt)).rejects.toBe(unknownError);
+    });
+  });
+  describe('sendBPM', () => {
+    const mockHeartBeatDTO: HeartBeatDTO = {
+      bpm: 72,
+    };
+    const mockHeartBeatResponseDTO: HeartBeatResponseDTO = {
+      ...mockHeartBeatDTO,
+      updatedAt: date,
+    };
+    it('happy path: returns the bpm and updatedAt returned by prisma and sends the notification when the bpm is greater than 120 e the caregiverId exist', async () => {
+      (prisma.elderProfile?.update as jest.Mock).mockResolvedValueOnce({
+        bpm: 121,
+        updatedAt: date,
+        caregiverId: 1,
+        user: {
+          name: 'johnDoe',
+        },
+      });
+      (pushToken.send as jest.Mock).mockResolvedValueOnce(null);
+
+      await expect(
+        service.sendBPM(mockPayloadJwt, { bpm: 121 }),
+      ).resolves.toEqual({ bpm: 121, updatedAt: date });
+      expect(prisma.elderProfile?.update).toHaveBeenCalledTimes(1);
+      expect(pushToken.send).toHaveBeenCalledTimes(1);
+    });
+    it('returns the bpm and updatedAt returned by prisma and does not send the notification when the bpm is less than or equal to 120', async () => {
+      (prisma.elderProfile?.update as jest.Mock).mockResolvedValueOnce(
+        mockHeartBeatResponseDTO,
+      );
+
+      await expect(
+        service.sendBPM(mockPayloadJwt, {
+          bpm: mockHeartBeatDTO.bpm,
+        }),
+      ).resolves.toEqual(mockHeartBeatResponseDTO);
+      expect(prisma.elderProfile?.update).toHaveBeenCalledTimes(1);
+      expect(pushToken.send).not.toHaveBeenCalled();
+    });
+    it('should throw if prisma returns bpm value equals null', async () => {
+      (prisma.elderProfile?.update as jest.Mock).mockResolvedValueOnce({
+        bpm: null,
+        updatedAt: date,
+      });
+
+      await expect(
+        service.sendBPM(mockPayloadJwt, mockHeartBeatDTO),
+      ).rejects.toThrow(InternalServerErrorException);
+      expect(prisma.elderProfile?.update).toHaveBeenCalledTimes(1);
+    });
+    it('should propagate not-found error from prisma (simulating P2025)', async () => {
+      const prismaNotFoundError = {
+        code: 'P2025',
+        message:
+          'An operation failed because it depends on one or more records that were required but not found.',
+      };
+      (prisma.elderProfile?.update as jest.Mock).mockRejectedValueOnce(
+        prismaNotFoundError,
+      );
+
+      await expect(
+        service.sendBPM(mockPayloadJwt, mockHeartBeatDTO),
+      ).rejects.toEqual(prismaNotFoundError);
+    });
+    it('should propagate other DB errors as InternalServerError (or original)', async () => {
+      const unknownError = new Error('connection error');
+      (prisma.elderProfile?.update as jest.Mock).mockRejectedValueOnce(
+        unknownError,
+      );
+
+      await expect(
+        service.sendBPM(mockPayloadJwt, mockHeartBeatDTO),
       ).rejects.toBe(unknownError);
     });
   });
