a7271f5db40686b24e40cdf913c3bef80ab25980
William01br <williamstcorreia123@gmail.com>
2026-04-02 17:26:19 -0300

    refactor: delete env.config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,13 @@
-import { InternalServerErrorException, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { PushTokenModule } from './push-token/push-token.module';
-import { envSchema } from '../src/config/env.config';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [
-    AuthModule,
-    PrismaModule,
-    UsersModule,
-    PushTokenModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validate: (config) => {
-        const parsed = envSchema.safeParse(config);
-        if (!parsed.success) {
-          console.error('Invalid environment variables:', parsed.error.issues);
-          throw new InternalServerErrorException(
-            'Invalid environment variables',
-          );
-        }
-        return parsed.data;
-      },
-    }),
-  ],
+  imports: [AuthModule, PrismaModule, UsersModule, PushTokenModule],
 })
 export class AppModule {}
